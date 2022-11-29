d11bb1c8e3692fdca2e853dd9e5890e73d683b37
Casey Brugna <cbrugna1@binghamton.edu>
2022-11-29 17:05:20 -0500

    Fixed initial

--- proxyserver1.py
+++ proxyserver1.py
@@ -1,3 +1,20 @@
+"""
+Notes
+
+- Client <=> proxy <=> webserver 
+- Use project 1 multithreading 
+
+Proxy server should print:
+proxy-forward, DESTINATION (client or server), THREAD_ID, TIMESTAMP 
+
+Modify webserver to print: 
+server-response, THREAD-ID, TIMESTAMP
+
+If on same device, code and proxy code should be placed and run from separate directories
+
+"""
+
+
 from socket import *
 import sys
 
--- webserver1.py
+++ webserver1.py
@@ -1,15 +1,70 @@
-"""
-Notes
+# import socket module
+from socket import *
 
-- Client <=> proxy <=> webserver 
-- Use project 1 multithreading 
+# In order to terminate the program
+import sys
+from threading import *
+import threading
 
-Proxy server should print:
-proxy-forward, DESTINATION (client or server), THREAD_ID, TIMESTAMP 
+class ClientThread(Thread):
+    def __init__(self, connectionSocket):
+        Thread.__init__(self)
+        #self.connectionSocket = socket
+        print("New server socket thread created" + str(threading.get_native_id()))
+    
+    def run(self):
+        try:
+            message = connectionSocket.recv(1024) ### YOUR CODE HERE ###
+        
+            print('Message is: ', message)
 
-Modify webserver to print: 
-server-response, THREAD-ID, TIMESTAMP
+            filename = message.split()[1]
+            f = open(filename[1:])
+            outputdata = f.read() ### YOUR CODE HERE ###
 
-If on same device, code and proxy code should be placed and run from separate directories
+            # Send one HTTP header line into socket
+            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode()) ### YOUR CODE HERE ###
 
-"""
+            # Send the content of the requested file into socket
+            for i in range(0, len(outputdata)):
+                connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+
+            # Close client socket
+            connectionSocket.close()
+        except IOError:
+            # Send response message for file not found
+            connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode()) 
+            connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode()) ### YOUR CODE HERE ###
+            connectionSocket.close()
+
+
+# Prepare a sever socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+### YOUR CODE HERE ###
+serverPort = 960
+serverSocket.bind(("", serverPort))
+threads = []
+
+
+serverSocket.listen(4)
+
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    newthread = ClientThread(connectionSocket)
+    newthread.start()
+    threads.append(newthread)
+    
+
+        # Close client socket
+     ### YOUR CODE HERE ###
+
+# Close server socket
+
+serverSocket.close()
+
+# Terminate the program after sending the corresponding data
+sys.exit()
