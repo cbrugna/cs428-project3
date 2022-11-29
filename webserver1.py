98ac3e077de9de3f29e79e7054cd876fc87aca7d
Casey Brugna <cbrugna1@binghamton.edu>
2022-11-29 16:46:22 -0500

    Initial commit

---
+++ webserver1.py
@@ -0,0 +1,15 @@
+"""
+Notes
+
+- Client <=> proxy <=> webserver 
+- Use project 1 multithreading 
+
+Proxy server should print:
+proxy-forward, DESTINATION (client or server), THREAD_ID, TIMESTAMP 
+
+Modify webserver to print: 
+server-response, THREAD-ID, TIMESTAMP
+
+If on same device, code and proxy code should be placed and run from separate directories
+
+"""
